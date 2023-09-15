896a90ebeeb286dfa4fc704464e817e2e202be85
azamtariq1 <muhammadazamtariq4@gmail.com>
2023-09-14 21:15:27 -0700

    azam

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+let personname = "Muhammad Azam Tariq";
+let age = 25;
+let student = true;
+console.log(personname, age, student);
---
+++ index.ts
@@ -0,0 +1,4 @@
+let personname:string="Muhammad Azam Tariq"
+let age:number=25
+let student:boolean=true
+console.log(personname,age,student);
